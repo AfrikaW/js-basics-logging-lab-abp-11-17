4f876fb505e830df32c0bc2bbdb7ae1ae5746cb8
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-12 04:05:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const driver = 'bob';
+// invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument
+console.log("this code was called");
+
+console.log('bob');
+
+console.log('bob', 'is the driver variable name')
